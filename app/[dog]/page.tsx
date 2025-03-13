2cfbf150a9a2d60c3f29787f7c46ccfc8bc3a85a
Rio Kuchlyan <riokuchlyan@gmail.com>
2025-03-13 19:14:51 -0400

    Edit [dog]/page.tsx

--- app/[dog]/page.tsx
+++ app/[dog]/page.tsx
@@ -1,4 +1,55 @@
+import { FC } from "react";
 import Image from "next/image"
+import '../animations.css';
+
+interface PageProps {
+    params: { dog: string };
+}
+
+interface BreedImage{
+  message: string[];
+  status: string;
+}
+
+
+
+const DynamicPage: FC<PageProps> = async ({ params }) => {
+  const awaitedParams = await params;
+  const { dog } = awaitedParams;
+
+
+  const breedPics: BreedImage[] = [];
+  const response = await fetch(`https://dog.ceo/api/breed/${dog}/images`, { cache: 'no-store' });
+  var data: BreedImage = await response.json();
+  breedPics.push(data);
+
+  return (
+    <div className="fade-in">
+    <h1 className="text-center text-4xl mb-8">{dog}</h1>
+    <div id="dogList" className="flex flex-col items-center justify-center">
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-2">
+            {breedPics.map((breedPic, index) =>
+                breedPic.message.map((pic) => (
+                    <li key={`${pic}-${index}`} className="p-4 rounded shadow text-center">
+                        <Image
+                            className="rounded-lg w-[500px] h-[300px] object-cover"
+                            src={pic}
+                            alt="Dog"
+                            width={600}
+                            height={500}
+                        />
+                    </li>
+                ))
+            )}
+        </ul>
+        </div>
+</div>
+  );
+};
+
+export default DynamicPage;
+
+/*import Image from "next/image"
 import '../animations.css';
 
 interface BreedImage{
@@ -42,4 +93,4 @@ export default async function Page({ params }: param) {
             </div>
     </div>
   );
-}
+}*/
