6d982f66a4a45f39bf8c0f4c7ea5548826ddfac6
Rio Kuchlyan <riokuchlyan@gmail.com>
2025-03-14 22:06:35 -0400

    Test dogs page

---
+++ app/[dog]/page.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import Image from "next/image"
+import '../animations.css';
+
+export default function Page({ params }: { params: { dog: string } }) {
+    return (
+
+    <div>
+    <h1>{`${params.dog}`}</h1>
+    </div>
+    );
+}
