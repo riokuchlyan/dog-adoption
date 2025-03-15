23b668f9286b853cdf462bfd41a6faccbe422177
Rio Kuchlyan <riokuchlyan@gmail.com>
2025-03-14 22:19:10 -0400

    Implement dynamic routing

--- app/[dog]/page.tsx
+++ app/[dog]/page.tsx
@@ -1,10 +1,42 @@
+'use client'
+import Image from "next/image"
 import '../animations.css';
+import { usePathname } from 'next/navigation'
 
-export default function Page({ params }: { params: { dog: string } }) {
-    return (
-
-    <div>
-    <h1>{`${params.dog}`}</h1>
-    </div>
-    );
+interface BreedImage{
+  message: string[];
+  status: string;
 }
+
+const DynamicPage = async () => {
+    const dog  = usePathname().substring(1);
+    const breedPics: BreedImage[] = [];
+    const response = await fetch(`https://dog.ceo/api/breed/${dog}/images`, { cache: 'no-store' });
+    const data: BreedImage = await response.json();
+    breedPics.push(data);
+
+    return (
+        <div className="fade-in">
+            <h1 className="text-center text-4xl mb-8">{dog}</h1>
+            <div id="dogList" className="flex flex-col items-center justify-center">
+                <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-2">
+                    {breedPics.map((breedPic, index) =>
+                        breedPic.message.map((pic) => (
+                            <li key={`${pic}-${index}`} className="p-4 rounded shadow text-center">
+                                <Image
+                                    className="rounded-lg w-[500px] h-[300px] object-cover"
+                                    src={pic}
+                                    alt="Dog"
+                                    width={600}
+                                    height={500}
+                                />
+                            </li>
+                        ))
+                    )}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default DynamicPage;
